fc5f7506f8b82afcc0d05e78e563f2ac84ea8d1b
Michael Neuling <mikey@neuling.org>
2020-04-23 14:28:08 +1000

    Add calls to dis/enable potato uart IRQ
    Signed-off-by: Michael Neuling <mikey@neuling.org>

--- hello_world/console.h
+++ hello_world/console.h
@@ -1,6 +1,8 @@
 #include <stddef.h>
 
 void potato_uart_init(void);
+void potato_uart_irq_en(void);
+void potato_uart_irq_dis(void);
 int getchar(void);
 void putchar(unsigned char c);
 void putstr(const char *str, unsigned long len);
