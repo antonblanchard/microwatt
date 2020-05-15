3b603402d2f454ad3084956e50d9430d7b373ed1
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2020-05-15 13:15:48 +1000

    soc_reset: Use counters, add synchronizers
    In some cases we need to keep the reset held for much longer,
    so use counters rather than shift registers.
    
    Additionally, some signals such as ext_rst and pll_locked
    or signals going from the ext_clk domain to the pll_clk
    domain need to be treated as async, and testing them without
    synchronizers is asking for trouble.
    
    Finally, make the external reset also reset the PLL.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- fpga/soc_reset_tb.vhdl
+++ fpga/soc_reset_tb.vhdl
@@ -12,22 +12,22 @@ architecture behave of soc_reset_tb is
     signal ext_rst_in    : std_ulogic;
 
     signal pll_rst_out          : std_ulogic;
-    signal pll_rst_out_expected : std_ulogic;
     signal rst_out                : std_ulogic;
-    signal rst_out_expected       : std_ulogic;
 
     constant clk_period : time := 10 ns;
 
     type test_vector is record
         pll_locked_in : std_ulogic;
         ext_rst_in    : std_ulogic;
-        pll_rst_out : std_ulogic;
+        pll_rst_out   : std_ulogic;
         rst_out       : std_ulogic;
     end record;
 
     type test_vector_array is array (natural range <>) of test_vector;
     constant test_vectors : test_vector_array := (
         -- PLL not locked, reset button not pressed
+        ('0', '1', '1', '1'),
+        ('0', '1', '1', '1'),
         ('0', '1', '1', '1'),
         ('0', '1', '1', '1'),
         ('0', '1', '1', '1'),
@@ -41,15 +41,27 @@ architecture behave of soc_reset_tb is
         ('1', '1', '0', '1'),
         ('1', '1', '0', '1'),
         ('1', '1', '0', '1'),
+        ('1', '1', '0', '1'),
+        ('1', '1', '0', '1'),
         -- Finally SOC comes out of reset
         ('1', '1', '0', '0'),
         ('1', '1', '0', '0'),
 
         -- PLL locked, reset button pressed
-        ('1', '0', '0', '1'),
-        ('1', '0', '0', '1'),
-        ('1', '0', '0', '1'),
+        ('1', '0', '0', '0'),
+        ('1', '0', '0', '0'),
+        ('1', '0', '0', '0'),
+        ('1', '0', '1', '1'),
         -- PLL locked, reset button released
+        ('1', '1', '1', '1'),
+        ('1', '1', '1', '1'),
+        ('1', '1', '1', '1'),
+        ('1', '1', '1', '1'),
+        ('1', '1', '1', '1'),
+        ('1', '1', '1', '1'),
+        ('1', '1', '0', '1'),
+        ('1', '1', '0', '1'),
+        ('1', '1', '0', '1'),
         ('1', '1', '0', '1'),
         ('1', '1', '0', '1'),
         ('1', '1', '0', '1'),
@@ -59,8 +71,8 @@ architecture behave of soc_reset_tb is
 begin
     soc_reset_0: entity work.soc_reset
         generic map (
-            PLL_RESET_CLOCKS => 4,
-            SOC_RESET_CLOCKS => 4,
+            PLL_RESET_BITS => 2,
+            SOC_RESET_BITS => 2,
             RESET_LOW => true
             )
         port map (
@@ -83,17 +95,29 @@ begin
     end process clock;
 
     stim: process
+        variable tv : test_vector;
     begin
+        -- skew us a bit
+        wait for clk_period/4;
+
         for i in test_vectors'range loop
-            (pll_locked_in, ext_rst_in, pll_rst_out_expected, rst_out_expected) <= test_vectors(i);
+            tv := test_vectors(i);
 
-            --report "pll_locked_in " & std_ulogic'image(pll_locked_in);
-            --report "ext_rst_in " & std_ulogic'image(ext_rst_in);
-            --report "pll_rst_out " & std_ulogic'image(pll_rst_out);
-            --report "rst_out" & std_ulogic'image(rst_out);
+            pll_locked_in <= tv.pll_locked_in;
+            ext_rst_in <= tv.ext_rst_in;
 
-            assert pll_rst_out_expected = pll_rst_out report "pll_rst_out bad";
-            assert rst_out_expected = rst_out report "rst_out bad";
+            report " ** STEP " & integer'image(i);
+            report "pll_locked_in " & std_ulogic'image(pll_locked_in);
+            report "ext_rst_in " & std_ulogic'image(ext_rst_in);
+            report "pll_rst_out " & std_ulogic'image(pll_rst_out);
+            report "rst_out" & std_ulogic'image(rst_out);
+
+            assert tv.pll_rst_out = pll_rst_out report
+                "pll_rst_out bad exp="  & std_ulogic'image(tv.pll_rst_out) &
+                " got=" & std_ulogic'image(pll_rst_out);
+            assert tv.rst_out    =  rst_out     report
+                "rst_out bad exp=" & std_ulogic'image(tv.rst_out) &
+                " got=" & std_ulogic'image(rst_out);
 
             wait for clk_period;
         end loop;
