737ebd92f583d18f4b78c21d07de3f26edb57678
Jordan Niethe <jniethe5@gmail.com>
2020-07-08 14:34:42 +1000

    tests: Add tests for the PVR
    The PVR is a privileged read-only SPR. Test reading and writing in both
    supervisor and problem state. In supervisor state reading returns
    microwatt's assigned PVR number and writing is a noop. In problem state
    both reading and writing cause privileged instruction interrupts.
    
    Signed-off-by: Jordan Niethe <jniethe5@gmail.com>

--- tests/misc/misc.c
+++ tests/misc/misc.c
@@ -8,8 +8,12 @@
 #define PASS "PASS\n"
 #define FAIL "FAIL\n"
 
+#define PVR_MICROWATT 0x00630000
+
 extern long test_addpcis_1(void);
 extern long test_addpcis_2(void);
+extern long test_mfpvr(void);
+extern long test_mtpvr(void);
 
 // i < 100
 void print_test_number(int i)
@@ -35,6 +39,20 @@ int main(void)
 
 	print_test_number(2);
 	if (test_addpcis_2() != 0) {
+		fail = 1;
+		puts(FAIL);
+	} else
+		puts(PASS);
+
+	print_test_number(3);
+	if (test_mfpvr() != PVR_MICROWATT) {
+		fail = 1;
+		puts(FAIL);
+	} else
+		puts(PASS);
+
+	print_test_number(4);
+	if (test_mtpvr() != PVR_MICROWATT) {
 		fail = 1;
 		puts(FAIL);
 	} else
