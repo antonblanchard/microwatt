bd5e4bafba29b30b9cf8058740a1486d651b3c55
Dan Horák <dan@danny.cz>
2020-09-01 22:43:50 +0200

    hello_world: Debug print the gitinfo syscon register
       .oOOo.
     ."      ".
     ;  .mw.  ;   Microwatt, it works.
      . '  ' .
       \ || /     HDL Git SHA1: 211d23c4ad4fb5-dirty
        ;..;
        ;..;
        `ww'
    
    Signed-off-by: Dan Horák <dan@danny.cz>
    Signed-off-by: Michael Neuling <mikey@neuling.org>

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -2,24 +2,46 @@
 #include <stdbool.h>
 
 #include "console.h"
+#include "io.h"
+#include "microwatt_soc.h"
 
-static char mw_logo[] =
-
+static char mw_logo1[] =
 "\n"
 "   .oOOo.     \n"
 " .\"      \". \n"
 " ;  .mw.  ;   Microwatt, it works.\n"
 "  . '  ' .    \n"
-"   \\ || /    \n"
+"   \\ || /     HDL Git SHA1: ";
+
+static char mw_logo2[] =
+"\n"
 "    ;..;      \n"
 "    ;..;      \n"
-"    `ww'   \n";
+"    `ww'      \n\n";
 
 int main(void)
 {
+	uint64_t gitinfo;
+	uint8_t c;
+        bool dirty;
+
 	console_init();
 
-	puts(mw_logo);
+	puts(mw_logo1);
+
+	gitinfo = readq(SYSCON_BASE + SYS_REG_GIT_INFO);
+        dirty = gitinfo >> 63;
+	for (int i = 0; i < 14; i++) {
+		c = (gitinfo >> 52) & 0xf;
+		if (c >= 10)
+                        putchar(0x61 + c - 10); // a-f
+                else
+                        putchar(0x30 + c); // 0-9
+		gitinfo <<= 4;
+	}
+        if (dirty)
+                puts("-dirty");
+	puts(mw_logo2);
 
 	while (1) {
 		unsigned char c = getchar();
