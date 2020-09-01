005de209c6d5ccd1ef9dc48d2ef8d7c55ff05266
Dan Horák <dan@danny.cz>
2020-09-01 22:43:50 +0200

    hello_world: debug print of the gitinfo syscon register
    Signed-off-by: Dan Horák <dan@danny.cz>

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 
 #include "console.h"
+#include "io.h"
+#include "microwatt_soc.h"
 
 static char mw_logo[] =
 
@@ -17,9 +19,19 @@ static char mw_logo[] =
 
 int main(void)
 {
+	uint64_t gitinfo;
+	uint8_t c;
+
 	console_init();
 
 	puts(mw_logo);
+
+	gitinfo = readq(SYSCON_BASE + SYS_REG_GIT_INFO);
+	for (int i = 0; i < 16; i++) {
+		c = gitinfo % 16;
+		if (c >= 10) putchar(0x61 + c - 10); else putchar(0x30 + c);
+		gitinfo >>= 4;
+	}
 
 	while (1) {
 		unsigned char c = getchar();
