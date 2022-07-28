eeac86c9d8efa987e65a68dbc31541e7119a401b
Michael Neuling <mikey@neuling.org>
2022-07-28 15:29:25 +1000

    test: Add test for metavalues
    Make sure they don't increase in future
    
    Signed-off-by: Michael Neuling <mikey@neuling.org>

--- scripts/run_test_console.sh
+++ scripts/run_test_console.sh
@@ -23,7 +23,15 @@ cd $TMPDIR
 
 cp ${MICROWATT_DIR}/tests/${TEST}.bin main_ram.bin
 
-${MICROWATT_DIR}/core_tb > /dev/null 2> test1.out || true
+${MICROWATT_DIR}/core_tb > console.out 2> test1.out || true
+
+# check metavalues aren't increasing
+COUNT=$(grep -c 'metavalue' console.out)
+EXP=$(cat ${MICROWATT_DIR}/tests/${TEST}.metavalue)
+if [[ $COUNT -gt $EXP ]] ; then
+   echo "$TEST FAIL ******** metavalues increased from $EXP to $COUNT"
+   exit 1
+fi
 
 grep -v "Failed to bind debug socket" test1.out > test.out
 
@@ -31,5 +39,5 @@ cp ${MICROWATT_DIR}/tests/${TEST}.console_out exp.out
 
 diff -q test.out exp.out && echo "$TEST PASS" && exit 0
 
-echo "$TEST FAIL ********"
+echo "$TEST FAIL ******** Console output changed"
 exit 1
