antonblanchard/microwatt

commit bc4d02cb0dcc5b502a45651953ac7bd34521f0b9

Start removing SPRs from register file
This starts the process of removing SPRs from the register file by
moving SRR0/1, SPRG0-3, HSRR0/1 and HSPRG0/1 out of the register file
and putting them into execute1.  They are stored in a pair of small
RAM arrays, referred to as "even" and "odd".  The reason for having
two arrays is so that two values can be read and written in each
cycle.  For example, SRR0 and SRR1 can be written in parallel by an
interrupt and read in parallel by the rfid instruction.

The addresses in the RAM which will be accessed are determined in the
decode2 stage.  We have one write address for both sides, but two read
addresses, since in future we will want to be able to read CTR at the
same time as either LR or TAR.

We now have a connection from writeback to execute1 which carries the
partial SRR1 value for an interrupt.  SRR0 comes from the execute
pipeline; we no longer need to carry instruction addresses along the
LSU and FPU pipelines.  Since SRR0 and SRR1 can be written in the same
cycle now, we don't need the little state machine in writeback any
more.

Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

writeback.vhdl
@@ -25,20 +25,12 @@ entity writeback is
         events       : out WritebackEventType;
 
         flush_out    : out std_ulogic;
-        interrupt_out: out std_ulogic;
+        interrupt_out: out WritebackToExecute1Type;
         complete_out : out instr_tag_t
         );
 end entity writeback;
 
 architecture behaviour of writeback is
-    type irq_state_t is (WRITE_SRR0, WRITE_SRR1);
-
-    type reg_type is record
-        state : irq_state_t;
-        srr1  : std_ulogic_vector(63 downto 0);
-    end record;
-
-    signal r, rin : reg_type;
 
 begin
     writeback_0: process(clk)
@@ -47,13 +39,6 @@ begin
         variable w : std_ulogic_vector(0 downto 0);
     begin
         if rising_edge(clk) then
-            if rst = '1' then
-                r.state <= WRITE_SRR0;
-                r.srr1 <= (others => '0');
-            else
-                r <= rin;
-            end if;
-
             -- Do consistency checks only on the clock edge
             x(0) := e_in.valid;
             y(0) := l_in.valid;
@@ -82,7 +67,6 @@ begin
     end process;
 
     writeback_1: process(all)
-        variable v    : reg_type;
         variable f    : WritebackToFetch1Type;
         variable scf  : std_ulogic_vector(3 downto 0);
         variable vec  : integer range 0 to 16#fff#;
@@ -92,9 +76,7 @@ begin
         w_out <= WritebackToRegisterFileInit;
         c_out <= WritebackToCrFileInit;
         f := WritebackToFetch1Init;
-        interrupt_out <= '0';
         vec := 0;
-        v := r;
 
         complete_out <= instr_tag_init;
         if e_in.valid = '1' then
@@ -108,37 +90,21 @@ begin
         events.fp_complete <= fp_in.valid;
 
         intr := e_in.interrupt or l_in.interrupt or fp_in.interrupt;
+        interrupt_out.intr <= intr;
 
-        if r.state = WRITE_SRR1 then
-            w_out.write_reg <= fast_spr_num(SPR_SRR1);
-            w_out.write_data <= r.srr1;
-            w_out.write_enable <= '1';
-            interrupt_out <= '1';
-            v.state := WRITE_SRR0;
-
-        elsif intr = '1' then
-            w_out.write_reg <= fast_spr_num(SPR_SRR0);
-            w_out.write_enable <= '1';
-            v.state := WRITE_SRR1;
+        if intr = '1' then
             srr1 := (others => '0');
             if e_in.interrupt = '1' then
                 vec := e_in.intr_vec;
-                w_out.write_data <= e_in.last_nia;
                 srr1 := e_in.srr1;
             elsif l_in.interrupt = '1' then
                 vec := l_in.intr_vec;
-                w_out.write_data <= l_in.srr0;
                 srr1 := l_in.srr1;
             elsif fp_in.interrupt = '1' then
                 vec := fp_in.intr_vec;
-                w_out.write_data <= fp_in.srr0;
                 srr1 := fp_in.srr1;
             end if;
-            v.srr1(63 downto 31) := e_in.msr(63 downto 31);
-            v.srr1(30 downto 27) := srr1(14 downto 11);
-            v.srr1(26 downto 22) := e_in.msr(26 downto 22);
-            v.srr1(21 downto 16) := srr1(5 downto 0);
-            v.srr1(15 downto 0) := e_in.msr(15 downto 0);
+            interrupt_out.srr1 <= srr1;
 
         else
             if e_in.write_enable = '1' then
@@ -229,6 +195,5 @@ begin
         wb_bypass.tag.valid <= complete_out.valid and w_out.write_enable;
         wb_bypass.data <= w_out.write_data;
 
-        rin <= v;
     end process;
 end;

common.vhdl
@@ -124,6 +124,28 @@ package common is
     end record;
     constant xerc_init : xer_common_t := (others => '0');
 
+    -- Some SPRs are stored in a pair of small RAMs in execute1
+    -- Even half:
+    subtype ramspr_index is natural range 0 to 7;
+    constant RAMSPR_SRR0   : ramspr_index := 0;
+    constant RAMSPR_HSRR0  : ramspr_index := 1;
+    constant RAMSPR_SPRG0  : ramspr_index := 2;
+    constant RAMSPR_SPRG2  : ramspr_index := 3;
+    constant RAMSPR_HSPRG0 : ramspr_index := 4;
+    -- Odd half:
+    constant RAMSPR_SRR1   : ramspr_index := 0;
+    constant RAMSPR_HSRR1  : ramspr_index := 1;
+    constant RAMSPR_SPRG1  : ramspr_index := 2;
+    constant RAMSPR_SPRG3  : ramspr_index := 3;
+    constant RAMSPR_HSPRG1 : ramspr_index := 4;
+
+    type ram_spr_info is record
+        index : ramspr_index;
+        isodd : std_ulogic;
+        valid : std_ulogic;
+    end record;
+    constant ram_spr_info_init: ram_spr_info := (index => 0, others => '0');
+
     subtype spr_selector is std_ulogic_vector(2 downto 0);
     type spr_id is record
         sel   : spr_selector;
@@ -253,12 +275,13 @@ package common is
         br_pred: std_ulogic; -- Branch was predicted to be taken
         big_endian: std_ulogic;
         spr_info : spr_id;
+        ram_spr : ram_spr_info;
     end record;
     constant Decode1ToDecode2Init : Decode1ToDecode2Type :=
         (valid => '0', stop_mark => '0', nia => (others => '0'), insn => (others => '0'),
          ispr1 => (others => '0'), ispr2 => (others => '0'), ispro => (others => '0'),
          decode => decode_rom_init, br_pred => '0', big_endian => '0',
-         spr_info => spr_id_init);
+         spr_info => spr_id_init, ram_spr => ram_spr_info_init);
 
     type Decode1ToFetch1Type is record
         redirect     : std_ulogic;
@@ -320,6 +343,13 @@ package common is
         repeat : std_ulogic;                            -- set if instruction is cracked into two ops
         second : std_ulogic;                            -- set if this is the second op
         spr_select : spr_id;
+        spr_is_ram : std_ulogic;
+        ramspr_even_rdaddr : ramspr_index;
+        ramspr_odd_rdaddr  : ramspr_index;
+        ramspr_rd_odd      : std_ulogic;
+        ramspr_wraddr      : ramspr_index;
+        ramspr_write_even  : std_ulogic;
+        ramspr_write_odd   : std_ulogic;
     end record;
     constant Decode2ToExecute1Init : Decode2ToExecute1Type :=
 	(valid => '0', unit => NONE, fac => NONE, insn_type => OP_ILLEGAL, instr_tag => instr_tag_init,
@@ -333,6 +363,9 @@ package common is
          cr => (others => '0'), insn => (others => '0'), data_len => (others => '0'),
          result_sel => "000", sub_select => "000",
          repeat => '0', second => '0', spr_select => spr_id_init,
+         spr_is_ram => '0',
+         ramspr_even_rdaddr => 0, ramspr_odd_rdaddr => 0, ramspr_rd_odd => '0',
+         ramspr_wraddr => 0, ramspr_write_even => '0', ramspr_write_odd => '0',
          others => (others => '0'));
 
     type MultiplyInputType is record
@@ -574,7 +607,6 @@ package common is
         store_done : std_ulogic;
         interrupt : std_ulogic;
         intr_vec : intr_vector_t;
-        srr0: std_ulogic_vector(63 downto 0);
         srr1: std_ulogic_vector(15 downto 0);
     end record;
     constant Loadstore1ToWritebackInit : Loadstore1ToWritebackType :=
@@ -582,7 +614,7 @@ package common is
          write_reg => (others => '0'), write_data => (others => '0'),
          xerc => xerc_init, rc => '0', store_done => '0',
          interrupt => '0', intr_vec => 0,
-         srr0 => (others => '0'), srr1 => (others => '0'));
+         srr1 => (others => '0'));
 
     type Loadstore1EventType is record
         load_complete  : std_ulogic;
@@ -675,7 +707,6 @@ package common is
         write_xerc      : std_ulogic;
         xerc            : xer_common_t;
         intr_vec        : intr_vector_t;
-        srr0            : std_ulogic_vector(63 downto 0);
         srr1            : std_ulogic_vector(15 downto 0);
     end record;
     constant FPUToWritebackInit : FPUToWritebackType :=
@@ -731,6 +762,11 @@ package common is
 							       write_cr_mask => (others => '0'),
 							       write_cr_data => (others => '0'));
 
+    type WritebackToExecute1Type is record
+        intr : std_ulogic;
+        srr1 : std_ulogic_vector(15 downto 0);
+    end record;
+
     type WritebackEventType is record
         instr_complete : std_ulogic;
         fp_complete    : std_ulogic;
@@ -755,26 +791,6 @@ package body common is
            n := 0;              -- N.B. decode2 relies on this specific value
        when SPR_CTR =>
            n := 1;              -- N.B. decode2 relies on this specific value
-       when SPR_SRR0 =>
-           n := 2;
-       when SPR_SRR1 =>
-           n := 3;
-       when SPR_HSRR0 =>
-           n := 4;
-       when SPR_HSRR1 =>
-           n := 5;
-       when SPR_SPRG0 =>
-           n := 6;
-       when SPR_SPRG1 =>
-           n := 7;
-       when SPR_SPRG2 =>
-           n := 8;
-       when SPR_SPRG3 | SPR_SPRG3U =>
-           n := 9;
-       when SPR_HSPRG0 =>
-           n := 10;
-       when SPR_HSPRG1 =>
-           n := 11;
        when SPR_TAR =>
            n := 13;
        when others =>

core.vhdl
@@ -102,6 +102,7 @@ architecture behave of core is
 
     -- Writeback signals
     signal writeback_bypass: bypass_data_t;
+    signal wb_interrupt: WritebackToExecute1Type;
 
     -- local signals
     signal fetch1_stall_in : std_ulogic;
@@ -122,7 +123,6 @@ architecture behave of core is
     signal complete: instr_tag_t;
     signal terminate: std_ulogic;
     signal core_rst: std_ulogic;
-    signal do_interrupt: std_ulogic;
 
     -- Delayed/Latched resets and alt_reset
     signal rst_fetch1  : std_ulogic;
@@ -361,7 +361,7 @@ begin
             l_in => loadstore1_to_execute1,
             fp_in => fpu_to_execute1,
             ext_irq_in => ext_irq,
-            interrupt_in => do_interrupt,
+            interrupt_in => wb_interrupt,
             l_out => execute1_to_loadstore1,
             fp_out => execute1_to_fpu,
             e_out => execute1_to_writeback,
@@ -469,7 +469,7 @@ begin
             f_out => writeback_to_fetch1,
             wb_bypass => writeback_bypass,
             events => writeback_events,
-            interrupt_out => do_interrupt,
+            interrupt_out => wb_interrupt,
             complete_out => complete
             );
 

decode1.vhdl
@@ -181,7 +181,7 @@ architecture behaviour of decode1 is
         -- isync
         2#111#    =>       (ALU, NONE, OP_ISYNC,     NONE,       NONE,        NONE, NONE, '0', '0', '0', '0', ZERO, '0', NONE, '0', '0', '0', '0', '0', '0', NONE, '0', '0', NONE),
         -- rfid
-        2#101#    =>       (ALU, NONE, OP_RFID,      SPR,        SPR,         NONE, NONE, '0', '0', '0', '0', ZERO, '0', NONE, '0', '0', '0', '0', '0', '0', NONE, '0', '0', NONE),
+        2#101#    =>       (ALU, NONE, OP_RFID,      NONE,       NONE,        NONE, NONE, '0', '0', '0', '0', ZERO, '0', NONE, '0', '0', '0', '0', '0', '0', NONE, '0', '0', NONE),
         others   => illegal_inst
         );
 
@@ -525,6 +525,42 @@ architecture behaviour of decode1 is
     constant nop_instr      : decode_rom_t := (ALU,  NONE, OP_NOP,          NONE,       NONE,        NONE, NONE, '0', '0', '0', '0', ZERO, '0', NONE, '0', '0', '0', '0', '0', '0', NONE, '0', '0', NONE);
     constant fetch_fail_inst: decode_rom_t := (LDST, NONE, OP_FETCH_FAILED, NONE,       NONE,        NONE, NONE, '0', '0', '0', '0', ZERO, '0', NONE, '0', '0', '0', '0', '0', '0', NONE, '0', '0', NONE);
 
+    function decode_ram_spr(sprn : spr_num_t) return ram_spr_info is
+        variable ret : ram_spr_info;
+    begin
+        ret := (index => 0, isodd => '0', valid => '1');
+        case sprn is
+            when SPR_SRR0 =>
+                ret.index := RAMSPR_SRR0;
+            when SPR_SRR1 =>
+                ret.index := RAMSPR_SRR1;
+                ret.isodd := '1';
+            when SPR_HSRR0 =>
+                ret.index := RAMSPR_HSRR0;
+            when SPR_HSRR1 =>
+                ret.index := RAMSPR_HSRR1;
+                ret.isodd := '1';
+            when SPR_SPRG0 =>
+                ret.index := RAMSPR_SPRG0;
+            when SPR_SPRG1 =>
+                ret.index := RAMSPR_SPRG1;
+                ret.isodd := '1';
+            when SPR_SPRG2 =>
+                ret.index := RAMSPR_SPRG2;
+            when SPR_SPRG3 | SPR_SPRG3U =>
+                ret.index := RAMSPR_SPRG3;
+                ret.isodd := '1';
+            when SPR_HSPRG0 =>
+                ret.index := RAMSPR_HSPRG0;
+            when SPR_HSPRG1 =>
+                ret.index := RAMSPR_HSPRG1;
+                ret.isodd := '1';
+            when others =>
+                ret.valid := '0';
+        end case;
+        return ret;
+    end;
+
     function map_spr(sprn : spr_num_t) return spr_id is
         variable i : spr_id;
     begin
@@ -614,6 +650,7 @@ begin
 
         sprn := decode_spr_num(f_in.insn);
         v.spr_info := map_spr(sprn);
+        v.ram_spr := decode_ram_spr(sprn);
 
         case to_integer(unsigned(majorop)) is
         when 4 =>
@@ -632,17 +669,17 @@ begin
 
             if std_match(f_in.insn(10 downto 1), "01-1010011") then
                 -- mfspr or mtspr
-                if is_fast_spr(v.ispr1) = '0' then
-                    -- Make mtspr to slow SPRs single issue
+                -- Make mtspr to slow SPRs single issue
+                if v.spr_info.valid = '1' then
                     vi.force_single := f_in.insn(8);
-                    -- send MMU-related SPRs to loadstore1
-                    case sprn is
-                        when SPR_DAR | SPR_DSISR | SPR_PID | SPR_PTCR =>
-                            vi.override_decode.unit := LDST;
-                            vi.override_unit := '1';
-                        when others =>
-                    end case;
                 end if;
+                -- send MMU-related SPRs to loadstore1
+                case sprn is
+                    when SPR_DAR | SPR_DSISR | SPR_PID | SPR_PTCR =>
+                        vi.override_decode.unit := LDST;
+                        vi.override_unit := '1';
+                    when others =>
+                end case;
             end if;
 
         when 16 =>
@@ -690,10 +727,6 @@ begin
                 else
                     v.ispr2 := fast_spr_num(SPR_TAR);
                 end if;
-            else
-                -- Could be OP_RFID
-                v.ispr1 := fast_spr_num(SPR_SRR1);
-                v.ispr2 := fast_spr_num(SPR_SRR0);
             end if;
 
         when 24 =>

decode2.vhdl
@@ -480,6 +480,23 @@ begin
 
             v.e.spr_select := d_in.spr_info;
 
+            case op is
+                when OP_MFSPR =>
+                    v.e.ramspr_even_rdaddr := d_in.ram_spr.index;
+                    v.e.ramspr_odd_rdaddr := d_in.ram_spr.index;
+                    v.e.ramspr_rd_odd := d_in.ram_spr.isodd;
+                    v.e.spr_is_ram := d_in.ram_spr.valid;
+                when OP_MTSPR =>
+                    v.e.ramspr_wraddr := d_in.ram_spr.index;
+                    v.e.ramspr_write_even := d_in.ram_spr.valid and not d_in.ram_spr.isodd;
+                    v.e.ramspr_write_odd := d_in.ram_spr.valid and d_in.ram_spr.isodd;
+                    v.e.spr_is_ram := d_in.ram_spr.valid;
+                when OP_RFID =>
+                    v.e.ramspr_even_rdaddr := RAMSPR_SRR0;
+                    v.e.ramspr_odd_rdaddr := RAMSPR_SRR1;
+                when others =>
+            end case;
+
             case d_in.decode.length is
                 when is1B =>
                     length := "0001";
@@ -530,6 +547,8 @@ begin
             if op = OP_MFSPR then
                 if is_fast_spr(d_in.ispr1) = '1' then
                     v.e.result_sel := "000";        -- adder_result, effectively a_in
+                elsif d_in.ram_spr.valid = '1' then
+                    v.e.result_sel := "101";        -- ramspr_result
                 elsif d_in.spr_info.valid = '0' then
                     -- Privileged mfspr to invalid/unimplemented SPR numbers
                     -- writes the contents of RT back to RT (i.e. it's a no-op)

commit fc58559ee82178ba3fa1a82530bd381fa9eb55ba

writeback: Eliminate unintentional inferred latch
By not assigning to interrupt_out.srr1 in some circumstances, the
writeback_1 process creates an inferred latch, which is not
desirable.  Eliminate it by restructuring the code so
interrupt_out.srr1 is always set, to zeroes if nothing else.

Fixes: bc4d02cb0dcc ("Start removing SPRs from register file", 2022-07-12)
Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

writeback.vhdl
@@ -92,21 +92,20 @@ begin
         intr := e_in.interrupt or l_in.interrupt or fp_in.interrupt;
         interrupt_out.intr <= intr;
 
-        if intr = '1' then
-            srr1 := (others => '0');
-            if e_in.interrupt = '1' then
-                vec := e_in.intr_vec;
-                srr1 := e_in.srr1;
-            elsif l_in.interrupt = '1' then
-                vec := l_in.intr_vec;
-                srr1 := l_in.srr1;
-            elsif fp_in.interrupt = '1' then
-                vec := fp_in.intr_vec;
-                srr1 := fp_in.srr1;
-            end if;
-            interrupt_out.srr1 <= srr1;
+        srr1 := (others => '0');
+        if e_in.interrupt = '1' then
+            vec := e_in.intr_vec;
+            srr1 := e_in.srr1;
+        elsif l_in.interrupt = '1' then
+            vec := l_in.intr_vec;
+            srr1 := l_in.srr1;
+        elsif fp_in.interrupt = '1' then
+            vec := fp_in.intr_vec;
+            srr1 := fp_in.srr1;
+        end if;
+        interrupt_out.srr1 <= srr1;
 
-        else
+        if intr = '0' then
             if e_in.write_enable = '1' then
                 w_out.write_reg <= e_in.write_reg;
                 w_out.write_data <= e_in.write_data;