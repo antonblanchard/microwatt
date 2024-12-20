antonblanchard/microwatt

commit c2da82764f746745ab00d75bba9dd66b4c40c98d

core: Implement CFAR register
This implements the CFAR SPR as a slow SPR stored in 'ctrl'.  Taken
branches and rfid update it to the address of the branch or rfid
instruction.

To simplify the logic, this makes rfid use the branch logic to
generate its redirect (requiring SRR0 to come in to execute1 on
the B input and SRR1 on the A input), and the masking of the bottom
2 bits of NIA is moved to fetch1.

Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

execute1.vhdl
@@ -652,26 +652,27 @@ begin
 		    result_en := '1';
 		    v.e.write_reg := fast_spr_num(SPR_CTR);
 		end if;
-		if ppc_bc_taken(bo, bi, e_in.cr, a_in) = '1' then
-		    f_out.redirect <= '1';
-		    f_out.redirect_nia <= b_in(63 downto 2) & "00";
-		end if;
+                is_branch := '1';
+		taken_branch := ppc_bc_taken(bo, bi, e_in.cr, a_in);
+                abs_branch := '1';
 
 	    when OP_RFID =>
-		f_out.redirect <= '1';
-                f_out.virt_mode <= b_in(MSR_IR) or b_in(MSR_PR);
-                f_out.priv_mode <= not b_in(MSR_PR);
-		f_out.redirect_nia <= a_in(63 downto 2) & "00"; -- srr0
+                f_out.virt_mode <= a_in(MSR_IR) or a_in(MSR_PR);
+                f_out.priv_mode <= not a_in(MSR_PR);
                 -- Can't use msr_copy here because the partial function MSR
                 -- bits should be left unchanged, not zeroed.
-                ctrl_tmp.msr(63 downto 31) <= b_in(63 downto 31);
-                ctrl_tmp.msr(26 downto 22) <= b_in(26 downto 22);
-                ctrl_tmp.msr(15 downto 0)  <= b_in(15 downto 0);
-                if b_in(MSR_PR) = '1' then
+                ctrl_tmp.msr(63 downto 31) <= a_in(63 downto 31);
+                ctrl_tmp.msr(26 downto 22) <= a_in(26 downto 22);
+                ctrl_tmp.msr(15 downto 0)  <= a_in(15 downto 0);
+                if a_in(MSR_PR) = '1' then
                     ctrl_tmp.msr(MSR_EE) <= '1';
                     ctrl_tmp.msr(MSR_IR) <= '1';
                     ctrl_tmp.msr(MSR_DR) <= '1';
                 end if;
+                -- mark this as a branch so CFAR gets updated
+                is_branch := '1';
+                taken_branch := '1';
+                abs_branch := '1';
 
             when OP_CNTZ =>
                 v.e.valid := '0';
@@ -757,6 +758,8 @@ begin
 			spr_val(31 downto 0)  := ctrl.tb(63 downto 32);
 		    when SPR_DEC =>
 			spr_val := ctrl.dec;
+                    when SPR_CFAR =>
+                        spr_val := ctrl.cfar;
                     when 724 =>     -- LOG_ADDR SPR
                         spr_val := log_wr_addr & r.log_addr_spr;
                     when 725 =>     -- LOG_DATA SPR
@@ -879,9 +882,9 @@ begin
 	    v.e.rc := e_in.rc and valid_in;
 
             -- Mispredicted branches cause a redirect
-            if is_branch = '1' and taken_branch /= e_in.br_pred then
-                f_out.redirect <= '1';
+            if is_branch = '1' then
                 if taken_branch = '1' then
+                    ctrl_tmp.cfar <= e_in.nia;
                     if abs_branch = '1' then
                         f_out.redirect_nia <= b_in;
                     else
@@ -889,6 +892,9 @@ begin
                     end if;
                 else
                     f_out.redirect_nia <= next_nia;
+                end if;
+                if taken_branch /= e_in.br_pred then
+                    f_out.redirect <= '1';
                 end if;
             end if;
 

common.vhdl
@@ -31,6 +31,7 @@ package common is
     constant SPR_DEC    : spr_num_t := 22;
     constant SPR_SRR0   : spr_num_t := 26;
     constant SPR_SRR1   : spr_num_t := 27;
+    constant SPR_CFAR   : spr_num_t := 28;
     constant SPR_HSRR0  : spr_num_t := 314;
     constant SPR_HSRR1  : spr_num_t := 315;
     constant SPR_SPRG0  : spr_num_t := 272;
@@ -94,6 +95,7 @@ package common is
 	tb: std_ulogic_vector(63 downto 0);
 	dec: std_ulogic_vector(63 downto 0);
 	msr: std_ulogic_vector(63 downto 0);
+        cfar: std_ulogic_vector(63 downto 0);
 	irq_state : irq_state_t;
 	irq_nia: std_ulogic_vector(63 downto 0);
 	srr1: std_ulogic_vector(63 downto 0);

decode1.vhdl
@@ -473,8 +473,8 @@ begin
                 end if;
             else
                 -- Could be OP_RFID
-                v.ispr1 := fast_spr_num(SPR_SRR0);
-                v.ispr2 := fast_spr_num(SPR_SRR1);
+                v.ispr1 := fast_spr_num(SPR_SRR1);
+                v.ispr2 := fast_spr_num(SPR_SRR0);
             end if;
 
         elsif majorop = "011110" then

fetch1.vhdl
@@ -83,11 +83,11 @@ begin
             v.priv_mode := '1';
 	    v_int.stop_state := RUNNING;
 	elsif e_in.redirect = '1' then
-	    v.nia := e_in.redirect_nia;
+	    v.nia := e_in.redirect_nia(63 downto 2) & "00";
             v.virt_mode := e_in.virt_mode;
             v.priv_mode := e_in.priv_mode;
         elsif d_in.redirect = '1' then
-            v.nia := d_in.redirect_nia;
+            v.nia := d_in.redirect_nia(63 downto 2) & "00";
 	elsif stall_in = '0' then
 
 	    -- For debug stop/step to work properly we need a little bit of

commit ff0744b7957d8c309fb30328a5aa29056d1d0b94

execute1: Make CFAR able to be written using mtspr and read using DMI debug
mtspr to CFAR is currently a no-op, which is not what should happen.
Make it set the contents of CFAR.

Also provide access to CFAR via the DMI debug interface as register 0x31.

Fixes: c2da82764f74 ("core: Implement CFAR register", 2020-06-15)
Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

core_debug.vhdl
@@ -333,6 +333,9 @@ begin
                 when 5x"10" =>
                     isram := '0';
                     sel := SPRSEL_HEIR;
+                when 5x"11" =>
+                    isram := '0';
+                    sel := SPRSEL_CFAR;
                 when others =>
                     valid := '0';
             end case;

execute1.vhdl
@@ -79,6 +79,7 @@ architecture behaviour of execute1 is
         write_xerlow : std_ulogic;
         write_dec : std_ulogic;
         write_cfar : std_ulogic;
+        set_cfar : std_ulogic;
         write_loga : std_ulogic;
         inc_loga : std_ulogic;
         write_pmuspr : std_ulogic;
@@ -687,6 +688,8 @@ begin
                                 dbg_spr_data <= assemble_hfscr(ctrl);
                             when SPRSEL_HEIR =>
                                 dbg_spr_data <= ctrl.heir;
+                            when SPRSEL_CFAR =>
+                                dbg_spr_data <= ctrl.cfar;
                             when others =>
                                 dbg_spr_data <= assemble_xer(xerc_in, ctrl.xer_low);
                         end case;
@@ -1177,7 +1180,7 @@ begin
                 if ex1.msr(MSR_BE) = '1' then
                     v.do_trace := '1';
                 end if;
-                v.se.write_cfar := '1';
+                v.se.set_cfar := '1';
             when OP_BC =>
                 -- If CTR is being decremented, it is in ramspr_odd.
 		bo := insn_bo(e_in.insn);
@@ -1196,7 +1199,7 @@ begin
                 if ex1.msr(MSR_BE) = '1' then
                     v.do_trace := '1';
                 end if;
-                v.se.write_cfar := v.take_branch;
+                v.se.set_cfar := v.take_branch;
             when OP_BCREG =>
                 -- If CTR is being decremented, it is in ramspr_odd.
                 -- The target address is in ramspr_result (LR, CTR or TAR).
@@ -1209,7 +1212,7 @@ begin
                 if ex1.msr(MSR_BE) = '1' then
                     v.do_trace := '1';
                 end if;
-                v.se.write_cfar := v.take_branch;
+                v.se.set_cfar := v.take_branch;
 
 	    when OP_RFID =>
                 srr1 := ramspr_odd;
@@ -1229,7 +1232,7 @@ begin
                 end if;
                 v.se.write_msr := '1';
                 v.e.redirect := '1';
-                v.se.write_cfar := '1';
+                v.se.set_cfar := '1';
                 if HAS_FPU then
                     v.fp_intr := fp_in.exception and
                                  (srr1(MSR_FE0) or srr1(MSR_FE1));
@@ -1323,6 +1326,8 @@ begin
                             v.se.write_dec := '1';
                         when SPRSEL_LOGA =>
                             v.se.write_loga := '1';
+                        when SPRSEL_CFAR =>
+                            v.se.write_cfar := '1';
                         when SPRSEL_FSCR =>
                             v.se.write_fscr := '1';
                         when SPRSEL_HFSCR =>
@@ -1902,6 +1907,8 @@ begin
                 ctrl_tmp.dec <= ex1.e.write_data;
             end if;
             if ex1.se.write_cfar = '1' then
+                ctrl_tmp.cfar <= ex1.e.write_data;
+            elsif ex1.se.set_cfar = '1' then
                 ctrl_tmp.cfar <= ex1.e.last_nia;
             end if;
             if ex1.se.write_loga = '1' then

scripts/mw_debug/mw_debug.c
@@ -550,7 +550,7 @@ static const char *fast_spr_names[] =
 	"lr", "ctr", "srr0", "srr1", "hsrr0", "hsrr1",
 	"sprg0", "sprg1", "sprg2", "sprg3",
 	"hsprg0", "hsprg1", "xer", "tar",
-	"fscr", "hfscr", "heir",
+	"fscr", "hfscr", "heir", "cfar",
 };
 
 static const char *ldst_spr_names[] = {